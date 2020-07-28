6646e05727d8b9356698277d9a4a67d4fb8b3a49
Lucas Fernandes <lfrenandes@hotmail.com>
2020-07-28 12:04:53 -0300

    Page default

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/Logo.png';
 import './Menu.css';
 import Button from '../Button';
+// import ButtonLink from './components/ButtonLink';
 
-function Menu (){
-    return (
-        <nav className ="Menu">
-            <a href="/">
-                <img className="Logo" src = {Logo} alt="Lulu Flix logo"/>
-            </a>
-            <Button as="a" className="ButtonLink" href="/">
-                Novo Vídeo
-            </Button>
-            
-        </nav>
+function Menu() {
+  return (
+    <nav className="Menu">
+      <Link to="/">
+        <img className="Logo" src={Logo} alt="luluFlix logo" />
+      </Link>
 
-    );
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
